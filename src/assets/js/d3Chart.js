99488e9bdcc23d09e40af91a57f8fecd50b522c3
Tony Odom <todom86@gmail.com>
2021-02-07 22:07:14 -0700

    housekeeping

--- src/assets/js/d3Chart.js
+++ src/assets/js/d3Chart.js
@@ -1,11 +1,12 @@
+// Prep SVG area
 var svgWidth = 600;
-var svgHeight = 500;
+var svgHeight = 600;
 
 var margin = {
-    top: 60,
+    top: 110,
     right: 10,
     bottom: 180,
-    left: 20
+    left: 25
 };
 
 var width = svgWidth - margin.left - margin.right;
@@ -20,6 +21,8 @@ var svg = d3
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+
+// Function to get needed data by state
 function getStateInfo(data, state, chosenXAxis, chosenCategory) {
     var stateInfo = [];
 
@@ -49,6 +52,8 @@ function getStateInfo(data, state, chosenXAxis, chosenCategory) {
     return stateInfo
 }
 
+
+// Functions to render selected chart
 function xScale(stateInfo) {
     var xBandScale = d3.scaleBand()
         .domain(stateInfo.map(d => d.category))
@@ -100,16 +105,15 @@ function renderXAxis(newXScale, xAxis, chartGroup) {
 //     return yAxis
 // }
 
-function renderRect(rectangles, xAxis, newXScale, newYScale, stateInfo, barGroup) {
+function renderRect(rectangles, newXScale, newYScale, stateInfo, barGroup) {
       
     rectangles.transition()
         .delay((d,i) => i*50)
         .duration(1000)
         .attr("y", height)
+        .attr("width", 0)
         .attr("height", 0)
         .remove();
-    
-    // console.log(xAxis);
 
     var rectangles = barGroup.selectAll("rect")
         .data(stateInfo, d => JSON.stringify(d))
@@ -134,16 +138,7 @@ function renderRect(rectangles, xAxis, newXScale, newYScale, stateInfo, barGroup
 }
 
 function renderRectText(rectText, newXScale, newYScale, stateInfo, barGroup) {
-
-    // rectText
-    //   .data(stateInfo)
-    //   .transition()
-    //   .duration(500)
-    //   .delay((d,i) => i*100)
-    //   .attr("y", d => newYScale(d.percent)-2)
-    //   .text(d => format(d.percent) + "%");
-
-    // return rectText   
+   
     var format = d3.format(".2f"); 
     
     rectText.transition()
@@ -172,28 +167,45 @@ function renderRectText(rectText, newXScale, newYScale, stateInfo, barGroup) {
     return rectText
 }
 
+// Create spider graph based on selected data
 function updateSpider(stateInfo) {
 
-    stateInfo.push(stateInfo[0]);
+    // stateInfo.push(stateInfo[0]);
+
+    // var info = [{
+    //     type: 'scatterpolar',
+    //     r: stateInfo.map(d => d.percent),
+    //     theta: stateInfo.map(d => d.category),
+    //     fill: 'toself'
+    //     }];
+        
+    // var layout = {
+    //     polar: {
+    //     radialaxis: {
+    //         visible: true,
+    //         range: [0, d3.max(stateInfo.map(d => d.percent))]
+    //     }
+    //     },
+    //     showlegend: false
+    // };
 
     var info = [{
-        type: 'scatterpolar',
-        r: stateInfo.map(d => d.percent),
-        theta: stateInfo.map(d => d.category),
-        fill: 'toself'
-        }];
-        
+        type: 'pie',
+        values: stateInfo.map(d => d.percent),
+        labels: stateInfo.map(d => d.category),
+        textinfo: "label+percent",
+        textposition: "outside",
+        automargin: true
+    }];
+
     var layout = {
-        polar: {
-        radialaxis: {
-            visible: true,
-            range: [0, d3.max(stateInfo.map(d => d.percent))]
-        }
-        },
+        height: 400,
+        width: 400,
+        margin: {"t": 0, "b": 0, "l": 0, "r": 0},
         showlegend: false
     };
     
-    Plotly.newPlot("SpiderChart", info, layout);
+    Plotly.newPlot("pieChart", info, layout);
 }
     
 var url = 'http://localhost:5000/data'
@@ -203,16 +215,25 @@ d3.json(url).then((data) => {
     
     // console.log(data);
 
+    // Getting unique values from data
     var stateList = Array.from(new Set(data.map(row => row.state).sort()));
     var sexList = Array.from(new Set(data.map(row => row.sex).sort()));
     var ageList = Array.from(new Set(data.map(row => row.age_group).sort()));
-    var generationList = Array.from(new Set(data.map(row => row.generation).sort()));
-    var incomeList = Array.from(new Set(data.map(row => row.income).sort()));
-    var partyList = Array.from(new Set(data.map(row => row.party).sort()));
+    // var generationList = Array.from(new Set(data.map(row => row.generation).sort()));
+    var generationList = ["Greatest generation", "Silent Generation", "Baby Boomers",
+            "Generation X", "Millennials", "Don't know/refused"];
+    // var incomeList = Array.from(new Set(data.map(row => row.income).sort()));
+    var incomeList = ["Less than $10,000","10 to under $20,000","20 to under $30,000",
+            "30 to under $40,000","40 to under $50,000","50 to under $75,000","75 to under $100,000",
+            "100 to under $150,000","$150,000 or more"];
+    // var partyList = Array.from(new Set(data.map(row => row.party).sort()));
+    var partyList = ["Democrat","Republican","Independent","No preference","Other party", "Don't know/Refused"];
     var religionList = Array.from(new Set(data.map(row => row.religion).sort()));
-    var ideoList = Array.from(new Set(data.map(row => row.ideo).sort()));
+    // var ideoList = Array.from(new Set(data.map(row => row.ideo).sort()));
+    var ideoList = ["Very liberal","Liberal","Moderate","Conservative","Very conservative", "Don't know/Refused"];
 
-    var categoryArray = ['Religion', 'Sex', 'Age Group', 'Generation', 'Income', 'Party', 'Ideology'];
+    // Array to hold category values to display in dropdown menu
+    var categoryArray = ['Ideology', 'Sex', 'Age Group', 'Generation', 'Income', 'Party', 'Religion'];
 
     // console.log(stateList);
     // console.log(sexList);
@@ -223,10 +244,12 @@ d3.json(url).then((data) => {
     // console.log(religionList);
     // console.log(ideoList);
 
-    var chosenXAxis = 'religion';
+    // Initial values
+    var chosenXAxis = 'ideo';
     var chosenState = 'Alabama';
-    var chosenCategory = religionList;
+    var chosenCategory = ideoList;
 
+    // Append data to dropdowns
     var StateDropdown = d3.select("#selectState");
     var categoryDropdown = d3.select("#selectCategory");
 
@@ -244,6 +267,7 @@ d3.json(url).then((data) => {
             .property("value", d);
     });
 
+    // Render initial bar chart
     var stateInfo = getStateInfo(data, chosenState, chosenXAxis, chosenCategory);
 
     var xBandScale = xScale(stateInfo);
@@ -300,8 +324,10 @@ d3.json(url).then((data) => {
         .duration(1000)
         .attr("y", d => yLinearScale(d.percent)-5);
 
+    // Initial Spider Graph
     updateSpider(stateInfo);
             
+    // Update charts based on selected state
     d3.select("#selectState").on("change", function() {
         
         chosenState = d3.select("#selectState").node().value;
@@ -317,13 +343,14 @@ d3.json(url).then((data) => {
 
         // yAxis = renderYAxis(yLinearScale, yAxis, chartGroup);
 
-        rectangles = renderRect(rectangles, xAxis, xBandScale, yLinearScale, stateInfo, barGroup);
+        rectangles = renderRect(rectangles, xBandScale, yLinearScale, stateInfo, barGroup);
 
         rectText = renderRectText(rectText, xBandScale, yLinearScale, stateInfo, barGroup);
 
         updateSpider(stateInfo);
     });
     
+    // Update charts based on selected category
     d3.select("#selectCategory").on("change", function() {
         
         newCategory = d3.select("#selectCategory").node().value;
@@ -364,7 +391,7 @@ d3.json(url).then((data) => {
 
         // yAxis = renderYAxis(yLinearScale, yAxis, chartGroup);
 
-        rectangles = renderRect(rectangles, xAxis, xBandScale, yLinearScale, stateInfo, barGroup);
+        rectangles = renderRect(rectangles, xBandScale, yLinearScale, stateInfo, barGroup);
 
         rectText = renderRectText(rectText, xBandScale, yLinearScale, stateInfo, barGroup);
 
